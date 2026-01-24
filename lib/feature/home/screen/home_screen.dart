40bad2cac7f739585b26dba45b9faadf74ecd406
Amgad <amgonemilwork@gmail.com>
2026-01-24 22:04:27 +0200

    upadte packages

--- lib/feature/home/screen/home_screen.dart
+++ lib/feature/home/screen/home_screen.dart
@@ -1,87 +1,247 @@
 import 'package:flutter/material.dart';
+import 'package:nemo/res/app_colors.dart';
+import 'package:nemo/res/text_style.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the HomeScreen object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("Home"),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: AppColors.scaffoldBackground,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 20),
+                    // Back button and title
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        GestureDetector(
+                          onTap: () => Navigator.pop(context),
+                          child: const Icon(
+                            Icons.arrow_back,
+                            size: 24,
+                            color: AppColors.onSurface,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                        Text(
+                          'Contact us',
+                          style: AppTextStyles.headline3.copyWith(
+                            color: AppColors.onSurface,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    // Subtitle
+                    Text(
+                      "Don't hesitate to contact us whether you have a suggestion on our improvement, a complain to discuss or an issue to solve.",
+                      style: AppTextStyles.field2.copyWith(
+                        color: AppColors.onSurface.withOpacity(0.7),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    // Call us and Email us cards
+                    Row(
+                      children: [
+                        Expanded(
+                          child: _ContactCard(
+                            icon: Icons.call,
+                            title: 'Call us',
+                            subtitle: 'Our team is on the line',
+                            time: 'Mon-Fri  •  9-17',
+                          ),
+                        ),
+                        const SizedBox(width: 17),
+                        Expanded(
+                          child: _ContactCard(
+                            icon: Icons.mail,
+                            title: 'Email us',
+                            subtitle: 'Our team is on the line',
+                            time: 'Mon-Fri  •  9-17',
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 36),
+                    // Contact us on Social Media
+                    Text(
+                      'Contact us on Social Media',
+                      style: AppTextStyles.field2.copyWith(
+                        color: AppColors.onSurface.withOpacity(0.7),
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                  ],
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24),
+                    child: Column(
+                      children: [
+                        _SocialMediaButton(
+                          icon: Icons.chat,
+                          label: 'WhatsApp',
+                          onTap: () {},
+                        ),
+                        const SizedBox(height: 16),
+                        _SocialMediaButton(
+                          icon: Icons.facebook,
+                          label: 'Facebook',
+                          onTap: () {},
+                        ),
+                        const SizedBox(height: 16),
+                        _SocialMediaButton(
+                          icon: Icons.camera_alt,
+                          label: 'Instagram',
+                          onTap: () {},
+                        ),
+                        const SizedBox(height: 16),
+                        _SocialMediaButton(
+                          icon: Icons.send,
+                          label: 'Telegram',
+                          onTap: () {},
+                        ),
+                        const SizedBox(height: 40),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _ContactCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final String time;
+
+  const _ContactCard({
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.time,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: AppColors.primary,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            size: 24,
+            color: AppColors.onPrimary,
+          ),
+          const SizedBox(height: 10),
+          Text(
+            title,
+            style: AppTextStyles.action2.copyWith(
+              color: AppColors.onPrimary,
+            ),
+          ),
+          const SizedBox(height: 16),
+          Text.rich(
+            TextSpan(
+              children: [
+                TextSpan(
+                  text: subtitle,
+                  style: AppTextStyles.field2.copyWith(
+                    color: AppColors.appBarTextColor,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                TextSpan(
+                  text: '\n$time',
+                  style: AppTextStyles.field2.copyWith(
+                    color: AppColors.appBarTextColor,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SocialMediaButton extends StatelessWidget {
+  final IconData icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const _SocialMediaButton({
+    required this.icon,
+    required this.label,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: AppColors.surface,
+            width: 1,
+          ),
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 16),
+        child: Row(
+          children: [
+            Container(
+              width: 24,
+              height: 24,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
+                borderRadius: BorderRadius.circular(6),
+              ),
+              child: Icon(
+                icon,
+                size: 16,
+                color: AppColors.onPrimary,
+              ),
+            ),
+            const SizedBox(width: 12),
+            Text(
+              label,
+              style: AppTextStyles.action3.copyWith(
+                color: AppColors.onSurface.withOpacity(0.7),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
